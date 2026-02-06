52a84d8ffa183e11284277c3c8e2b3221e30df4e
nayan <nayan8teen@gmail.com>
2026-02-06 08:07:00 -0500

    init descriptions

--- selfdrive/ui/sunnypilot/layouts/settings/cruise.py
+++ selfdrive/ui/sunnypilot/layouts/settings/cruise.py
@@ -7,7 +7,8 @@ See the LICENSE.md file in the root directory for more details.
 from enum import IntEnum
 
 from openpilot.common.params import Params
-from openpilot.system.ui.lib.multilang import tr
+from openpilot.selfdrive.ui.ui_state import ui_state
+from openpilot.system.ui.lib.multilang import tr, tr_noop
 from openpilot.system.ui.sunnypilot.widgets.list_view import toggle_item_sp, option_item_sp, simple_button_item_sp
 from openpilot.system.ui.widgets.scroller_tici import Scroller
 from openpilot.system.ui.widgets import Widget
@@ -15,6 +16,16 @@ from openpilot.system.ui.widgets import Widget
 class PanelType(IntEnum):
   CRUISE = 0
   SLA = 1
+
+ICBM_DESC = tr_noop("When enabled, sunnypilot will attempt to manage the built-in cruise control buttons by emulating button presses for limited longitudinal control.")
+ICMB_UNAVAILABLE = tr_noop("Intelligent Cruise Button Management is currently unavailable on this platform.")
+ICMB_UNAVAILABLE_LONG_AVAILABLE = tr_noop("Disable the sunnypilot Longitudinal Control (alpha) toggle to allow Intelligent Cruise Button Management.")
+ICMB_UNAVAILABLE_LONG_UNAVAILABLE = tr_noop("sunnypilot Longitudinal Control is the default longitudinal control for this platform.")
+
+ACC_ENABLED_DESCRIPTION = tr_noop("Enable custom Short & Long press increments for cruise speed increase/decrease.")
+ACC_NOLONG_DESCRIPTION = tr_noop("This feature can only be used with sunnypilot longitudinal control enabled.")
+ACC_PCMCRUISE_DISABLED_DESCRIPTION = tr_noop("This feature is not supported on this platform due to vehicle limitations.")
+ONROAD_ONLY_DESCRIPTION = tr_noop("Start the vehicle to check vehicle compatibility.")
 
 class CruiseLayout(Widget):
   def __init__(self):
@@ -97,3 +108,12 @@ class CruiseLayout(Widget):
 
   def _update_state(self):
     super()._update_state()
+
+    if ui_state.CP is not None and ui_state.CP_SP is not None:
+      if ui_state.CP_SP.intelligentCruiseButtonManagementAvailable and not ui_state.has_longitudinal_control:
+        self.icbm_toggle.set_enabled(ui_state.is_offroad())
+        self.icbm_toggle.set_description(tr(ICBM_DESC))
+      else:
+        ui_state.params.remove("IntelligentCruiseButtonManagement")
+        self.icbm_toggle.set_enabled(False)
+
